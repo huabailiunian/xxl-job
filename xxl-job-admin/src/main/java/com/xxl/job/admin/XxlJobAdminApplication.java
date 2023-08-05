d11700604219ffc318e6abba8058e5183fa7a9ae
youly <34894356@qq.com>
2023-08-05 16:53:04 +0800

    add apollo

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,5 +1,6 @@
 package com.xxl.job.admin;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xuxueli 2018-10-28 00:38:13
  */
 @SpringBootApplication
+@EnableApolloConfig
 public class XxlJobAdminApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(XxlJobAdminApplication.class, args);
-	}
+    }
 
 }
